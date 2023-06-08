1360ddf714089b8ac632b7e837be89d47a822a79
Vsollery <vsollery@gmail.com>
2023-06-08 23:36:29 +0700

    add conditional if there are no tasks

--- resources/views/tasks/todos/index.blade.php
+++ resources/views/tasks/todos/index.blade.php
@@ -8,50 +8,61 @@
     <div class="table-responsive">
         <a href="/dashboard/mytasks/create" class="btn btn-primary mb-3">Add New Task</a>
         <table class="table table-striped table-sm">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($tasks as $task)
+            @unless ($tasks->isEmpty())
+                <thead>
                     <tr>
-                        <td>{{ $loop->iteration }}</td>
-                        <td>{{ $task->title }}</td>
-                        <td>
-                            @if ($task->is_completed == 1)
-                                Finished
-                            @else
-                                Unfinised
-                            @endif
-                        </td>
-                        <td>
-                            <form action="/dashboard/mytasks/{{ $task->id }}" method="post" class="d-inline">
-                                @csrf
-                                <button class="badge bg-success border-0" onclick= "return confirm ('Finish this task?')"><span data-feather="check"
-                                    class="align-text-bottom"></span>
-                                </button>
-                            </form>
-                            {{-- <a href="/" class="badge bg-success"><span data-feather="check"
-                                    class="align-text-bottom"></span></i></a> --}}
-                            <a href="/dashboard/mytasks/{{ $task->id }}" class="badge bg-primary"> <span data-feather="eye"
-                                    class="align-text-bottom"></span></a>
-                            <a href="/" class="badge bg-warning"> <span data-feather="edit"
-                                    class="align-text-bottom"></span></a>
-                            <form action="/dashboard/posts/{{ $task->id }}" method="post" class="d-inline">
-                                @method('delete')
-                                @csrf
-                                <button class="badge bg-danger border-0"
-                                    onclick="return confirm('Are you sure you want to delet this post?')"><span
-                                        data-feather="trash" class="align-text-bottom"></span></button>
-                            </form>
-
-                        </td>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Check</th>
+                        <th scope="col">Action</th>
                     </tr>
-                @endforeach
+                </thead>
+                <tbody>
+                    @foreach ($tasks as $task)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $task->title }}</td>
+                            <td>
+                                @if ($task->is_completed == 1)
+                                    Finished
+                                @else
+                                    Unfinised
+                                @endif
+                            </td>
+                            <td>
+                                @if ($task->is_completed == 0)
+                                    <form action="/dashboard/mytasks/{{ $task->id }}" method="post" class="d-inline">
+                                        @csrf
+                                        <button class="badge bg-success border-0"
+                                            onclick="return confirm ('Finish this task?')"><span data-feather="check"
+                                                class="align-text-bottom"></span>
+                                        </button>
+                                    </form>
+                                @endif
+                            </td>
+                            <td>
+                                {{-- <a href="/" class="badge bg-success"><span data-feather="check"
+                                    class="align-text-bottom"></span></i></a> --}}
+                                <a href="/dashboard/mytasks/{{ $task->id }}" class="badge bg-primary"> <span
+                                        data-feather="eye" class="align-text-bottom"></span></a>
+                                <a href="/" class="badge bg-warning"> <span data-feather="edit"
+                                        class="align-text-bottom"></span></a>
+                                <form action="/dashboard/posts/{{ $task->id }}" method="post" class="d-inline">
+                                    @method('delete')
+                                    @csrf
+                                    <button class="badge bg-danger border-0"
+                                        onclick="return confirm('Are you sure you want to delet this post?')"><span
+                                            data-feather="trash" class="align-text-bottom"></span></button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                @else
+                    <div class="alert alert-primary" role="alert">
+                        You do not have any tasks
+                    </div>
+                @endunless
             </tbody>
         </table>
     </div>
--- resources/views/tasks/todos/unfinished.blade.php
+++ resources/views/tasks/todos/unfinished.blade.php
@@ -1,42 +1,58 @@
 @extends('tasks.layouts.main')
 
 @section('container')
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">My Unfinished Tasks</h1>
-</div>
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">My Unfinished Tasks</h1>
+    </div>
 
-<div class="table-responsive">
-    <table class="table table-striped table-sm">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Title</th>
-                <th scope="col">Status</th>
-                <th scope="col">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($tasks as $task)
-            <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $task->title }}</td>
-                <td>
-                    Unfinished
-                </td>
-                <td>
-                    <a href="/dashboard/mytasks/{{ $task->id }}" class="badge bg-primary"> <span data-feather="eye" class="align-text-bottom"></span></a>
-                    <a href="/dashboard/mytasks/{{ $task->id }}/edit" class="badge bg-warning"> <span data-feather="edit" class="align-text-bottom"></span></a>
-                    <form action="/dashboard/mytasks/{{ $task->id }}" method="post" class="d-inline">
-                        @method('delete')
-                        @csrf
-                        <button class="badge bg-danger border-0" onclick="return confirm('Are you sure you want to delet this task?')"><span data-feather="trash" class="align-text-bottom"></span></button>
-                    </form>
-                    
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+    <div class="table-responsive">
+        <table class="table table-striped table-sm">
+            @unless ($tasks->isEmpty())
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $task->title }}</td>
+                        <td>
+                            Unfinished
+                        </td>
+                        <td>
+                            <form action="/dashboard/mytasks/{{ $task->id }}" method="post" class="d-inline">
+                                @csrf
+                                <button class="badge bg-success border-0"
+                                    onclick="return confirm ('Finish this task?')"><span data-feather="check"
+                                        class="align-text-bottom"></span>
+                                </button>
+                            </form>
+                            <a href="/dashboard/mytasks/{{ $task->id }}" class="badge bg-primary"> <span
+                                    data-feather="eye" class="align-text-bottom"></span></a>
+                            <a href="/dashboard/mytasks/{{ $task->id }}/edit" class="badge bg-warning"> <span
+                                    data-feather="edit" class="align-text-bottom"></span></a>
+                            <form action="/dashboard/mytasks/{{ $task->id }}" method="post" class="d-inline">
+                                @method('delete')
+                                @csrf
+                                <button class="badge bg-danger border-0"
+                                    onclick="return confirm('Are you sure you want to delet this task?')"><span
+                                        data-feather="trash" class="align-text-bottom"></span></button>
+                            </form>
 
+                        </td>
+                    </tr>
+                    @endforeach
+                @else
+                     <div class="alert alert-success" role="alert">
+                        Congrats! you finished all your tasks!
+                    </div>
+                @endunless
+            </tbody>
+        </table>
+    </div>
 @endsection
