7da25c0ac1ee53439e1152038e4aa864fa081494
fateh-ark <akbarfatih92@gmail.com>
2023-06-09 18:40:29 +0700

    Final register page

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.main')
 
 @section('container')
+
 <div class="row justify-content-center">
     <div class="col-md-5">
         <main class="form-registration">
@@ -8,9 +9,35 @@
             <form action="/register" method="post" class="needs-validation" novalidate>
                 @csrf
 
-                @input('Name', 'name', 'text', 'rounded-top')
-                @input('Email', 'email', 'email')
-                @input('Password', 'password', 'password', 'rounded-bottom')
+                <div class="form-floating">
+                    <label for="inputName" class="sr-only">Name</label>
+                    <input type="text" id="inputName" name="name" class="form-control rounded-top @error('name') is-invalid @enderror" placeholder="Name" value="{{ old('name') }}" required>
+                    @error('name')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                </div>
+
+                <div class="form-floating">
+                    <label for="inputEmail" class="sr-only">Email</label>
+                    <input type="email" id="inputEmail" name="email" class="form-control @error('email') is-invalid @enderror"placeholder="Email" value="{{ old('name') }}" required>
+                    @error('email')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                </div>
+
+                <div class="form-floating">
+                    <label for="inputPassword" class="sr-only">Password</label>
+                    <input type="password" id="inputPassword" name="password" class="form-control rounded-bottom @error('password') is-invalid @enderror" placeholder="Password">
+                    @error('password')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                </div>
 
                 <button class="btn btn-lg btn-primary btn-block mt-3" type="submit">Register</button>
             </form>
@@ -18,17 +45,5 @@
         </main>
     </div>
 </div>
+
 @endsection
-
-@php
-function input($label, $name, $type, $class = '') {
-    $error = $errors->has($name) ? ' is-invalid' : '';
-    $message = $errors->has($name) ? $errors->first($name) : '';
-
-    echo '<div class="form-floating">';
-    echo '<label for="input' . ucfirst($name) . '" class="sr-only">' . $label . '</label>';
-    echo '<input type="' . $type . '" id="input' . ucfirst($name) . '" name="' . $name . '" class="form-control ' . $class . $error . '" placeholder="' . $label . '" value="{{ old(\'' . $name . '\') }}" required>';
-    echo $error ? '<div class="invalid-feedback">' . $message . '</div>' : '';
-    echo '</div>';
-}
-@endphp
