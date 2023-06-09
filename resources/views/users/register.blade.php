88f3c5cb242ddd96d5ec124f209f2158175468e8
fateh-ark <akbarfatih92@gmail.com>
2023-06-09 18:30:46 +0700

    initial register page

---
+++ resources/views/users/register.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.main')
+
+@section('container')
+    <div class="row justify-content-center">
+        <div class="col-md-5">
+            <main class="form-registration">
+                <h1 class="h3 mb-3 font-weight-normal text-center mb-3">Register</h1>
+                <form action="/register" method="post" class="needs-validation" novalidate>
+                    {{ csrf_field() }}
+
+                    <div class="form-floating">
+                        <label for="inputName" class="sr-only">Name</label>
+                        <input type="text" id="inputName" name="name" class="form-control rounded-top {{ $errors->has('name') ? 'is-invalid' : '' }}" placeholder="Name" value="{{ old('name') }}" required>
+                        @if ($errors->has('name'))
+                            <div class="invalid-feedback">
+                                {{ $errors->first('name') }}
+                            </div>
+                        @endif
+                    </div>
+
+                    <div class="form-floating">
+                        <label for="inputEmail" class="sr-only">Email</label>
+                        <input type="email" id="inputEmail" name="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" placeholder="Email" value="{{ old('email') }}" required>
+                        @if ($errors->has('email'))
+                            <div class="invalid-feedback">
+                                {{ $errors->first('email') }}
+                            </div>
+                        @endif
+                    </div>
+
+                    <div class="form-floating">
+                        <label for="inputPassword" class="sr-only">Password</label>
+                        <input type="password" id="inputPassword" name="password" class="form-control rounded-bottom {{ $errors->has('password') ? 'is-invalid' : '' }}" placeholder="Password">
+                        @if ($errors->has('password'))
+                            <div class="invalid-feedback">
+                                {{ $errors->first('password') }}
+                            </div>
+                        @endif
+                    </div>
+
+                    <button class="btn btn-lg btn-primary btn-block mt-3" type="submit">Register</button>
+                </form>
+                <small class="d-block text-center mt-3">Already have an account? <a href="/login">Login</a></small>
+            </main>
+        </div>
+    </div>
+@endsection
