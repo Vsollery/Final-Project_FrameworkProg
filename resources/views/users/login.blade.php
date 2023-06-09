1719229754287455837ba0fd340e3cd6ff891ffb
fateh-ark <akbarfatih92@gmail.com>
2023-06-09 18:02:50 +0700

    Login page initialized.

---
+++ resources/views/users/login.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.main')
+
+@section('container')
+<div class="row justify-content-center">
+    <div class="col-md-5">
+
+        @if(session()->has('success'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        @endif
+
+        @if(session()->has('loginError'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            {{ session('loginError') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        @endif
+
+        <main class="form-signin">
+            <h1 class="h3 mb-3 font-weight-normal text-center mb-3">Login</h1>
+            <form action="/login" method="post">
+                @csrf
+                <label for="inputEmail" class="sr-only">Email address</label>
+                <it type="email" id="inputEmail" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email address" value="{{ old('email') }}" required autofocus>
+                @error('email')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+                @enderror
+
+                <label for="inputPassword" class="sr-only">Password</label>
+                <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
+
+                <button class="btn btn-lg btn-primary btn-block mt-3" type="submit">Login</button>
+            </form>
+            <small class="d-block text-center mt-3">Don't have an account? <a href="/register">Register</a></small>
+        </main>
+    </div>
+</div>
+
+
+@endsection
