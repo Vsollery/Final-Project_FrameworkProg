e5e1f950d40ed94880e29f4957b30e4b2ab2d30e
fateh-ark <akbarfatih92@gmail.com>
2023-06-09 18:06:21 +0700

    Minor Fixes for Login Page

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -16,27 +16,7 @@
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
         @endif
-
-        <main class="form-signin">
-            <h1 class="h3 mb-3 font-weight-normal text-center mb-3">Login</h1>
-            <form action="/login" method="post">
-                @csrf
-                <label for="inputEmail" class="sr-only">Email address</label>
-                <it type="email" id="inputEmail" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email address" value="{{ old('email') }}" required autofocus>
-                @error('email')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
-                @enderror
-
-                <label for="inputPassword" class="sr-only">Password</label>
-                <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
-
-                <button class="btn btn-lg btn-primary btn-block mt-3" type="submit">Login</button>
-            </form>
-            <small class="d-block text-center mt-3">Don't have an account? <a href="/register">Register</a></small>
-        </main>
-    </div>
+        
 </div>
 
 
