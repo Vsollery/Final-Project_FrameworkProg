86265cbdb9854a52821ba42fb200e6bece73a96b
Vsollery <vsollery@gmail.com>
2023-06-14 18:12:18 +0700

    fix little bit of dashboard

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -17,8 +17,8 @@
                 <div class="card" style="width: 18rem;">
                     <div class="card-body">
                         <h5 class="card-title">Your Tasks</h5>
-                        <p class="card-text font-weight-bold">You have..</p>
-                        <a href="/dashboard/mytasks/" class="card-link">View Task</a>
+                        <p class="card-text font-weight-bold">You have {{ $tasks->count() }}</p>
+                        <a href="/dashboard/mytasks/" class="badge bg-primary" style="text-decoration:none">View Task</a>
                     </div>
                 </div>
             </div>
@@ -26,8 +26,8 @@
                 <div class="card" style="width: 18rem;">
                     <div class="card-body">
                         <h5 class="card-title">Your Finished Tasks</h5>
-                        <p class="card-text">You have .. Finished Tasks</p>
-                        <a href="/dashboard/mytasks/finished" class="card-link">View Task</a>
+                        <p class="card-text">You have {{ $tasks_complete->count() }} Finished Tasks</p>
+                        <a href="/dashboard/mytasks/finished" class="badge bg-primary" style="text-decoration:none">View Task</a>
                     </div>
                 </div>
             </div>
@@ -35,8 +35,8 @@
                 <div class="card" style="width: 18rem;">
                     <div class="card-body">
                         <h5 class="card-title">Your Unfinished Tasks</h5>
-                        <p class="card-text">You have .. Unfinished Tasks</p>
-                        <a href="/dashboard/mytasks/unfinished" class="card-link">View Task</a>
+                        <p class="card-text">You have {{ $tasks_incomplete->count() }} Unfinished Tasks</p>
+                        <a href="/dashboard/mytasks/unfinished" class="badge bg-primary" style="text-decoration:none">View Task</a>
                     </div>
                 </div>
             </div>
